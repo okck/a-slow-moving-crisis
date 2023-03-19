9f0c6ac0d707f170c24e1dc8a28502ba496eb339
Chris Kim <98981974+okck@users.noreply.github.com>
2023-03-19 19:36:04 -0400

    update sketch.js

--- sketch.js
+++ sketch.js
@@ -197,7 +197,7 @@ function draw() {
   }
   pop();
   shadowFrame.style.animation = 'blink 2s linear infinite';
-  shadowFrame.style.animationDelay = '1300ms';
+  shadowFrame.style.animationDelay = '1500ms';
 }
 
 // dashed lines
